b3e1e18677a80c29f3b14f30e11879918447a154
Ariel Merlo <ariel_merlo_90@hotmail.com>
2021-11-25 08:00:12 -0300

    Toggler para cambio de tema terminado.

--- main.js
+++ main.js
@@ -1,44 +1,50 @@
-    //Cambio de tema
-    const setTheme = (theme) => {
-        document.documentElement.className = theme;
-        localStorage.setItem('theme', theme);
-    }
-
-    const getTheme = () => {
-        const theme = localStorage.getItem('theme');
-        theme && setTheme(theme);
-    }
-
-    getTheme();
-
-//Toggler
-
+//Cambio de Tema
+// Select the button
 const chk = document.getElementById('chk');
+// Select the theme preference from localStorage
+const currentTheme = localStorage.getItem("theme");
 
+// If the current theme in localStorage is "dark"...
+if (currentTheme == "dark") {
+  // ...then use the .dark-theme class
+  document.body.classList.add("dark-theme");
+}
+
+// Listen for a click on the button 
 chk.addEventListener('change', () => {
-	document.body.classList.toggle(setTheme('dark'));
+  // Toggle the .dark-theme class on each click
+  document.body.classList.toggle('dark-theme');
+  
+  // Let's say the theme is equal to light
+  let theme = "light";
+  // If the body contains the .dark-theme class...
+  if (document.body.classList.contains("dark-theme")) {
+    // ...then let's make the theme dark
+    theme = "dark";
+  }
+  // Then save the choice in localStorage
+  localStorage.setItem("theme", theme);
 });
 
 
-
 //Formulario
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
